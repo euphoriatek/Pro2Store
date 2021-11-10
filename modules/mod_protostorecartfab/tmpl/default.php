ca1b92c81160fbaf97b6197002f062eb63640b29
Ray Lawlor <rayplawlor@gmail.com>
2021-11-10 09:37:31 +0000

    Bumped versions - fixed Fab

--- modules/mod_protostorecartfab/tmpl/default.php
+++ modules/mod_protostorecartfab/tmpl/default.php
@@ -10,6 +10,7 @@ defined('_JEXEC') or die;
 
 /** @var $params */
 /** @var $checkoutLink string */
+
 /** @var $count int */
 
 use Joomla\CMS\Uri\Uri;
@@ -20,10 +21,15 @@ $id = uniqid('yps_cartfab_module');
 
 <div id="<?= $id; ?>">
     <div class="uk-hidden@m">
-        <div class="uk-position-fixed uk-position-<?= $params->get('position'); ?>-<?= $params->get('side'); ?> uk-margin-small-<?= $params->get('position') === 'bottom' ? 'bottom' : 'top'; ?> uk-margin-small-<?= $params->get('side') === 'left' ? 'left' : 'right'; ?>" style="z-index: 1000">
+        <div class="uk-position-fixed uk-position-<?= $params->get('position'); ?>-<?= $params->get('side'); ?> uk-margin-small-<?= $params->get('position') === 'bottom' ? 'bottom' : 'top'; ?> uk-margin-small-<?= $params->get('side') === 'left' ? 'left' : 'right'; ?>"
+             style="z-index: 1000">
 
-
-            <a href="<?= $checkoutLink; ?>" class="uk-icon-button uk-button-<?= $params->get('button_type'); ?> uk-margin-small-right" style="width: <?= $params->get('size'); ?>px; height: <?= $params->get('size'); ?>px"><span id="yps_cart_fab_icon" uk-icon="icon: cart"></span>{{count}}</a>
+            <a href="<?= $checkoutLink; ?>"
+               class="uk-icon-button uk-button-<?= $params->get('button_type'); ?> uk-margin-small-right"
+               style="width: <?= $params->get('size'); ?>px; height: <?= $params->get('size'); ?>px">
+                <span id="yps_cart_fab_icon" uk-icon="icon: cart"></span>
+                {{count}}
+            </a>
 
         </div>
     </div>
@@ -32,7 +38,8 @@ $id = uniqid('yps_cartfab_module');
     const <?= $id; ?> = {
         data() {
             return {
-                count: <?= $count; ?>
+                count: <?= $count; ?>,
+                loading: false
             }
 
         },
@@ -41,7 +48,7 @@ $id = uniqid('yps_cartfab_module');
         },
         methods: {
             async fetchCartItems() {
-
+                console.log('TESTY');
                 this.loading = true;
 
                 const request = await fetch("<?= Uri::base(); ?>index.php?option=com_ajax&plugin=protostore_ajaxhelper&method=post&task=task&type=cart.update&format=raw", {
@@ -51,9 +58,7 @@ $id = uniqid('yps_cartfab_module');
                 const response = await request.json();
 
                 if (response.success) {
-                    this.cartItems = response.data.cartItems;
                     this.count = response.data.count;
-                    this.total = response.data.total;
                     this.loading = false;
                 } else {
                     UIkit.notification({
