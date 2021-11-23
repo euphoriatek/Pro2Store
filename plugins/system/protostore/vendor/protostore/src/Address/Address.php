ba68740316773749423b24a912c7b05699566e88
Ray Lawlor <rayplawlor@gmail.com>
2021-11-23 16:24:10 +0000

    Update Address.php
    Added conditional check on country call

--- plugins/system/protostore/vendor/protostore/src/Address/Address.php
+++ plugins/system/protostore/vendor/protostore/src/Address/Address.php
@@ -85,9 +85,13 @@ class Address
 
 		$this->zone_name          = AddressFactory::getZoneName($this->zone);
 		$country                  = AddressFactory::getCountry($this->country);
-		$this->country_name       = $country->country_name;
-		$this->country_isocode_2  = $country->country_isocode_2;
-		$this->country_isocode_3  = $country->country_isocode_3;
+
+		if($country) {
+			$this->country_name       = $country->country_name;
+			$this->country_isocode_2  = $country->country_isocode_2;
+			$this->country_isocode_3  = $country->country_isocode_3;
+		}
+
 		$this->address_as_csv     = AddressFactory::getAddressAsCSV($this);
 		$this->isAssignedShipping = AddressFactory::checkAssigned($this->id, 'shipping');
 		$this->isAssignedBilling  = AddressFactory::checkAssigned($this->id, 'billing');
