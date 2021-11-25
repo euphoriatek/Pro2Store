a6cd6375c2cc08a1f170f20fde0e45ac874384cb
Ray Lawlor <rayplawlor@gmail.com>
2021-11-25 12:50:44 +0000

    added faker autoload

--- administrator/components/com_protostore/sql/updates/mysql/2.0.0.sql
+++ administrator/components/com_protostore/sql/updates/mysql/2.0.0.sql
@@ -1,23 +1,3 @@
-CREATE TABLE `#__protostore_cart_item`
-(
-    `id`              int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `cart_id`         int(11)          NOT NULL,
-    `joomla_item_id`  int(11)                   DEFAULT NULL,
-    `variant_id`      int(11)                   DEFAULT NULL,
-    `item_options`    text,
-    `bought_at_price` int(11)                   DEFAULT NULL,
-    `added`           datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `order_id`        int(11)                   DEFAULT NULL,
-    `cookie_id`       int(11)                   DEFAULT NULL,
-    `user_id`         int(11)                   DEFAULT NULL,
-    `amount`          int(11)                   DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  ROW_FORMAT = DYNAMIC;
-
-
-
 CREATE TABLE `#__protostore_product_variant`
 (
     `id`         int(11) NOT NULL AUTO_INCREMENT,
