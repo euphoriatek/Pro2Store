92d405e8d2598a4a0cf68f714dbf83894c7ab9b6
Ray Lawlor <rayplawlor@gmail.com>
2022-01-27 15:58:38 +0000

    Update protostore.php
    Removed redundant code from main backend file

--- administrator/components/com_protostore/protostore.php
+++ administrator/components/com_protostore/protostore.php
@@ -25,35 +25,29 @@ if (!Factory::getUser()->authorise('core.manage', 'com_protostore'))
 	throw new NotAllowed(Text::_('JERROR_ALERTNOAUTHOR'), 403);
 }
 
-if(\Protostore\Setup\SetupFactory::isSetup()) {
-	include ( JPATH_ADMINISTRATOR . '/components/com_protostore/views/wrapper/bootstrapWrapper.php');
-	new bootstrapWrapper();
-} else {
-	include ( JPATH_ADMINISTRATOR . '/components/com_protostore/views/wrapper/bootstrapWrapper.php');
-	new bootstrapWrapper();
-}
-
+include(JPATH_ADMINISTRATOR . '/components/com_protostore/views/wrapper/bootstrapWrapper.php');
+new bootstrapWrapper();
 
 
 ?>
 
 <?php if (Version::MAJOR_VERSION === 3) : ?>
 
-<script>
-    // Remove Joomla admin headers and toolbars
-    jQuery(document).ready(function () {
-        jQuery('#isisJsData').hide();
-        jQuery('header').hide();
-    });
-    // remove system message for Uikit Replacement
-    jQuery(document).ready(function () {
-        jQuery('#system-message-container').hide();
-        jQuery('#j-main-container').removeClass('span10');
+    <script>
+        // Remove Joomla admin headers and toolbars
+        jQuery(document).ready(function () {
+            jQuery('#isisJsData').hide();
+            jQuery('header').hide();
+        });
+        // remove system message for Uikit Replacement
+        jQuery(document).ready(function () {
+            jQuery('#system-message-container').hide();
+            jQuery('#j-main-container').removeClass('span10');
 
-    });
+        });
 
-    // jQuery('link[rel=stylesheet][href*="isis/css/template"]').remove();
-</script>
+        // jQuery('link[rel=stylesheet][href*="isis/css/template"]').remove();
+    </script>
 
 <?php else : ?>
 
